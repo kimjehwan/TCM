4800ae6f9bf6270234d7f24ddf71d1a671293183
Gj <Gj@Gj-PC>
2018-03-03 23:37:30 +0900

    DB 쿼리 따로 클래스로 빼놓음 점수저장 메소드 생성

---
+++ src/DB/DBQuery.java
@@ -0,0 +1,141 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import main.Player;
+
+public class DBQuery 
+{
+	public boolean logIn(String id, String pw, boolean check) {
+		Connection conn = null; // DB연결된 상태(세션)을 담은 객체
+		PreparedStatement pstm = null; // SQL 문을 나타내는 객체
+		ResultSet rs = null; // 쿼리문을 날린것에 대한 반환값을 담을 객체
+
+		try {
+			// SQL 문장을 만들고 만약 문장이 질의어(SELECT문)라면
+			// 그 결과를 담을 ResulSet 객체를 준비한 후 실행시킨다.
+			String quary = "SELECT * FROM TCM_USER";
+
+			conn = DBConnection.getConnection();
+			pstm = conn.prepareStatement(quary);
+			rs = pstm.executeQuery();
+
+			while (rs.next()) {
+				String dbId = rs.getString(1);			
+				String dbPw = rs.getString(2);
+				int dbPscore = rs.getInt(3);
+				int dbSscore = rs.getInt(4);
+				int dbQscore = rs.getInt(5);
+
+
+				if(id.equals(dbId) && pw.equals(dbPw)) {
+					new Player(dbId,dbPscore,dbSscore,dbQscore);
+					System.out.println("로그인 성공");
+					check=true;
+					break;
+				}
+			}
+		} catch (SQLException sqle) {
+			System.out.println("SELECT문에서 예외 발생");
+			sqle.printStackTrace();
+
+		} finally {
+			// DB 연결을 종료한다.
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+		return check;
+	}
+	
+	public void signUp(String id, String pw, boolean check) {
+		Connection conn = null; // DB연결된 상태(세션)을 담은 객체
+        PreparedStatement pstm = null;  // SQL 문을 나타내는 객체
+        ResultSet rs = null;  // 쿼리문을 날린것에 대한 반환값을 담을 객체
+        
+        try {
+            // SQL 문장을 만들고 만약 문장이 질의어(SELECT문)라면
+            // 그 결과를 담을 ResulSet 객체를 준비한 후 실행시킨다.
+            String quary1 = "INSERT INTO TCM_USER VALUES ( '" + id + "', '" + pw + "',1000,1000,1000)";
+            String quary2 = "SELECT * FROM TCM_USER";
+
+            conn = DBConnection.getConnection();
+            pstm = conn.prepareStatement(quary2);
+            rs = pstm.executeQuery();
+            
+            while (rs.next()) {
+				String dbId = rs.getString(1);			
+
+				if(id.equals(dbId)) {
+					System.out.println("회원가입 실패");
+					System.out.println("현재 해당 아이디가 존재합니다.");
+					check=false;
+					break;
+				}
+           // System.out.println("회원가입 성공");
+            }
+            if(check) {
+            	System.out.println("회원가입 성공");
+            	conn.prepareStatement(quary1).executeQuery();              
+            }else {
+            	return;
+            }
+        } catch (SQLException sqle) {
+            System.out.println("INSERT문에서 예외 발생");
+            sqle.printStackTrace();
+            
+        }finally{
+            // DB 연결을 종료한다.
+            try{
+                if ( rs != null ){rs.close();}   
+                if ( pstm != null ){pstm.close();}   
+                if ( conn != null ){conn.close(); }
+            }catch(Exception e){
+                throw new RuntimeException(e.getMessage());
+            }            
+        }  
+	}
+	
+	public static void savePoker() {
+		Connection conn = null; // DB연결된 상태(세션)을 담은 객체
+        PreparedStatement pstm = null;  // SQL 문을 나타내는 객체
+        ResultSet rs = null;  // 쿼리문을 날린것에 대한 반환값을 담을 객체
+        
+        try {
+            // SQL 문장을 만들고 만약 문장이 질의어(SELECT문)라면
+            // 그 결과를 담을 ResulSet 객체를 준비한 후 실행시킨다.
+            String quary1 = "UPDATE TCM_USER SET PSCORE = " + Player.getpScore() + " WHERE ID = '" + Player.getId() + "'";
+
+            conn = DBConnection.getConnection();
+            pstm = conn.prepareStatement(quary1);
+            rs = pstm.executeQuery();
+            
+        } catch (SQLException sqle) {
+            System.out.println("INSERT문에서 예외 발생");
+            sqle.printStackTrace();
+            
+        }finally{
+            // DB 연결을 종료한다.
+            try{
+                if ( rs != null ){rs.close();}   
+                if ( pstm != null ){pstm.close();}   
+                if ( conn != null ){conn.close(); }
+            }catch(Exception e){
+                throw new RuntimeException(e.getMessage());
+            }            
+        }  
+	}
+}
